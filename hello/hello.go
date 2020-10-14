3aa17202657a43a6b14cb197ed84a39d72542137
Allan Vidal <allanfvc@gmail.com>
2020-10-14 13:56:38 -0300

    feat: increase greeting with french and spanish

--- hello/hello.go
+++ hello/hello.go
@@ -2,12 +2,34 @@ package hello
 
 import "fmt"
 
+const spanish = "Spanish"
+const french = "French"
 const englishHelloPrefix = "Hello, "
+const spanishHelloPrefix = "Hola, "
+const frenchHelloPrefix = "Salut, "
 
-func Hello(name string) string {
-	return englishHelloPrefix + name
+func Hello(name string, language string) string {
+	if name == "" {
+		name = "World"
+	}
+	if language == "Spanish" {
+		return "Hola, " + name
+	}
+	return greetingPrefix(language) + name
+}
+
+func greetingPrefix(language string) (prefix string) {
+	switch language {
+	case french:
+		prefix = frenchHelloPrefix
+	case spanish:
+		prefix = spanishHelloPrefix
+	default:
+		prefix = englishHelloPrefix
+	}
+	return
 }
 
 func main() {
-	fmt.Println(Hello("World"))
+	fmt.Println(Hello("World", "English"))
 }
